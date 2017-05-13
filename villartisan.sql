8629170657f985b4992bc175da66dc92de2c3a2b
Sodara NHEK <sodara.nhek@supinternet.fr>
2017-05-13 15:57:39 +0200

    completing the database villartisan.sql

--- villartisan.sql
+++ villartisan.sql
@@ -17,10 +17,121 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `blog_project`
+-- Base de données :  `vill_artisan`
 --
 
 -- --------------------------------------------------------
+--
+-- Structure de la table `event`
+--
+
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `id_artisan` int(11) NOT NULL,
+  `title_event` varchar(100) NOT NULL,
+  `text_event` varchar(500) NOT NULL,
+  `photo_event` varchar(150) NOT NULL,
+  `date_event` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `id_sender` int(11) NOT NULL,
+  `id_receiver` int(11) NOT NULL,
+  `text_message` varchar(300) NOT NULL,
+  `date_message` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `message`
+--
+
+INSERT INTO `message` (`id`, `id_sender`, `id_receiver`, `text_message`, `date_message`) VALUES
+(16, 15, 14, 'Bonjour toto', '2017-05-05'),
+(17, 15, 14, 'Ca va?', '2017-05-05'),
+(18, 14, 15, 'Oui Sodara', '2017-05-05'),
+(19, 14, 16, 'Bonjour quelqu\'un d\'autre', '2017-05-05'),
+(20, 15, 4, 'Yo', '2017-05-05');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `user_type` varchar(10) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `adress` varchar(100) NOT NULL,
+  `postcode` int(5) NOT NULL,
+  `user_village` varchar(100) NOT NULL,
+  `user_interet` varchar(200) NOT NULL,
+  `user_pic` varchar(200) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `user_type`, `lastname`, `firstname`, `password`, `mail`, `adress`, `postcode`, `user_village`, `user_interet`, `user_pic`) VALUES
+(14, 'villager', 'toto', 'toto', '$2y$10$c2FsdHlzYWx0eXNhbHR5cu/PLv5d9OsJeOkrZloMvlC8RzQyKsyoS', 'toto@toto.fr', '94 avenue parmentier', 4545, 'Charonne', '', ''),
+(15, 'villager', 'NHEK', 'Sodara', '$2y$10$c2FsdHlzYWx0eXNhbHR5cuJ9osWrdD098BB.G/zsVbRAKWDZYLzOi', 'sodara.nhek@supinternet.fr', '95 avenue parmentier', 75011, 'Bellevile', '', '');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
 --
 -- Structure de la table `articles`
