9b7ec9f5595de78f1e7518b11f70fc1f894a0cb2
Sodara NHEK <sodara.nhek@supinternet.fr>
2017-05-19 14:52:33 +0200

    ajax for chat

---
+++ web/script_ajax.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+
+
+    function htmlMessages(idreceiver,arrayMessages){
+        var res="";
+        for(var i=0;i<arrayMessages.length;i++){
+            if(arrayMessages[i].id_sender == idreceiver){
+                res+="<div class='his_message'>"+arrayMessages[i].text_message+"</div>";
+            }else{
+                res+="<div class='your_message'>"+arrayMessages[i].text_message+"</div>";
+            }
+        }
+        return res;
+    }
+    $(".form-submit").click(function () {
+        var message=$(this).prev().prev()[0].value;
+        var idReceiver=$(this).prev()[0].value;
+        var thiselem=$(this);
+        var data="text-message="+message+"&id-receiver="+idReceiver;
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "index.php?action=sendMessageTo", //Relative or absolute path to response.php file
+            data: data,
+            success: function (data) {
+                var res=htmlMessages(idReceiver,data.messages);
+                var chatcontainerElem=thiselem.parent().parent()[0].children[1];
+                chatcontainerElem.innerHTML=res;
+            },
+            error: function (data) {
+                console.log("ERROR");
+            }
+        });
+    });
+};
