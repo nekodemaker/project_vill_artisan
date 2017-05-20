e579d2b3fa0c9f9b0b73a35cff51eeb106aac1e7
Emmanuel Micard <emmanuel.micard@supinternet.fr>
2017-05-20 18:57:32 +0200

    added marker on map

--- web/markers.js
+++ web/markers.js
@@ -3,6 +3,6 @@ var markers = [
         "name": 'Arnaud Vasser',
         "lat": '48.862173871693635',
         "lng": '2.34133243560791',
-        "description": '<div class="infowindow_content"><img class="img_infowindow" src="web/assets/img/bijoutier.png" alt=""><br><h5>Nom prenom</h5><h6><b>Joillier</b></h6><p>7 rue du Louvre, 75001 Paris<br><br>9h-12h30  13h30–19h</p></div>'
+        "description": '<div class="infowindow_content"><img class="img_infowindow" src="web/assets/img/bijoutier.png" alt=""><br><a href="#"><h5>Elise Dupont</h5></a><h6><b>Joillier</b></h6><p>7 rue du Louvre, 75001 Paris<br><br>9h-12h30  13h30–19h</p></div>'
     }
 ];
