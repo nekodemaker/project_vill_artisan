b9aaf4951aba8672c028ffa6626a7e634d38217a
Emmanuel Micard <emmanuel.micard@supinternet.fr>
2017-05-20 19:56:32 +0200

    new navbar

--- web/markers.js
+++ web/markers.js
@@ -1,8 +1,8 @@
 var markers = [
     {
-        "name": 'Arnaud Vasser',
+        "name": 'Elise Dupont',
         "lat": '48.862173871693635',
         "lng": '2.34133243560791',
-        "description": '<div class="infowindow_content"><img class="img_infowindow" src="web/assets/img/bijoutier.png" alt=""><br><a href="#"><h5>Elise Dupont</h5></a><h6><b>Joillier</b></h6><p>7 rue du Louvre, 75001 Paris<br><br>9h-12h30  13h30–19h</p></div>'
+        "description": '<div class="infowindow_content"><img class="img_infowindow" src="web/assets/img/bijoutier.png" alt=""><br><h5>Elise Dupont</h5><h6><b>Joillier</b></h6><p>7 rue du Louvre, 75001 Paris<br>9h-12h30  13h30–19h</p><a class="redirect_crafter_profil" href="artisanProfile&id=21">voir son profil</a></div>'
     }
 ];
